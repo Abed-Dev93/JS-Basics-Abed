cf34931728bdc49de5fe6a6e448c12787314c849
Abdulrahman Ghassa <abdulrahman.ghassa93@gmail.com>
2023-08-30 21:10:16 +0300

    Step_4

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,8 @@
+function user() {
+    let first = document.getElementById("name").value;
+    let last = document.getElementById("surname").value;
+    let city = document.getElementById("city").value;
+
+    alert("Hello " + first + " " + last + " From " + city);
+}
+    
