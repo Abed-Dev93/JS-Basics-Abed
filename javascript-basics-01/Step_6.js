bef1fb60656bbc6893bd6147ff905102a14ab952
Abdulrahman Ghassa <abdulrahman.ghassa93@gmail.com>
2023-08-30 21:24:24 +0300

    Step_6

---
+++ javascript-basics-01/Step_6.js
@@ -0,0 +1,6 @@
+function user() {
+    let num1 = document.getElementById("first_number").value;
+    let num2 = document.getElementById("second_number").value;
+
+    alert("The remainder after we divided " + num1 + " with " + num2 +" is : " + num1 % num2);
+}
