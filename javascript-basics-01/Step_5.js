ba80a848c79db3ede2e935197bcc3cc66f00256a
Abdulrahman Ghassa <abdulrahman.ghassa93@gmail.com>
2023-08-30 21:16:36 +0300

    Step_5

---
+++ javascript-basics-01/Step_5.js
@@ -0,0 +1,6 @@
+function user() {
+    let num1 = document.getElementById("first_number").value;
+    let num2 = document.getElementById("second_number").value;
+
+    alert(num1 + " * " + num2 + " = " + num1*num2);
+}
