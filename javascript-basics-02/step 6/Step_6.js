7d338151149250b34da7cca44167bdf425712226
Abdulrahman Ghassa <abdulrahman.ghassa93@gmail.com>
2023-08-31 11:35:02 +0300

    Step 6

---
+++ javascript-basics-02/step 6/Step_6.js
@@ -0,0 +1,13 @@
+let image = document.querySelectorAll("img");
+
+for (let i=0; i<5; i++) {
+    image[i].addEventListener("mouseover", function() {
+        image[i].src = "images/image" + (i+1) + "_2.jpg";
+    });
+}
+
+for (let i=0; i<5; i++) {
+    image[i].addEventListener("mouseout", function() {
+        image[i].src= "images/image" + (i+1) + ".jpg";
+    });
+}
